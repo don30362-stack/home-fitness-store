a88936e1c7d2c5eee1a76d5fe45bea96c6131293
Wang <don30362@gmail.com>
2026-08-09 16:56:11 +0800

    chore: initialize project

---
+++ frontend/src/App.vue
@@ -0,0 +1,117 @@
+<!-- <script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style> -->
+
+<script>
+export default {
+  data() {
+    return {
+      message: '正在連接 Laravel...',
+    }
+  },
+
+  async mounted() {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/test')
+      const data = await response.json()
+
+      this.message = data.message
+    } catch (error) {
+      console.error(error)
+      this.message = 'Laravel API 連線失敗'
+    }
+  },
+}
+</script>
+
+<template>
+  <main class="container py-5">
+    <h1>Home Fitness Store</h1>
+
+    <div class="alert alert-success mt-4">
+      {{ message }}
+    </div>
+  </main>
+</template>
